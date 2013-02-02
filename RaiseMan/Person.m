542e021a4060f767aef88ced72d6c7d1f751bffb
Marcin Malinowski <orientman@gmail.com>
2013-02-02 22:48:38 +0100

    Zapisywanie do pliku (poprzez serializację)

--- RaiseMan/Person.m
+++ RaiseMan/Person.m
@@ -17,10 +17,27 @@
         return nil;
     }
 
-    expectedRaise = 5.0;
+    expectedRaise = 0.0;
     personName = @"New Person";
 
     return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super init];
+    if (self == nil) {
+        return nil;
+    }
+    personName = [aDecoder decodeObjectForKey:@"personName"];
+    expectedRaise = [aDecoder decodeFloatForKey:@"expectedRaise"];
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    [aCoder encodeObject:personName forKey:@"personName"];
+    [aCoder encodeFloat:expectedRaise forKey:@"expectedRaise"];
 }
 
 - (void)setNilValueForKey:(NSString *)key
